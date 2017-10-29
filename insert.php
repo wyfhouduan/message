c75d0d4c70c7f2024ae1bcd615027cc2ccda6cb3
wyfhouduan <32233591+wyfhouduan@users.noreply.github.com>
2017-10-29 14:31:16 +0800

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$id=$_POST["id"];
+$biaoti=$_POST["biaoti"];
+$xingming=$_POST["xingming"];
+$liuyan=$_POST["liuyan"];
+
+$sql="use liuyanban;INSERT into massage(id,biaoti,xingming,liuyan,lastdate)VALUES ('','$biaodan','$xingming','$liuyan','')";
+mysqli_query($conn,$sql);
+
+mysqli_close($conn);
+header("location:4.php");
+exit();
+
+?>
