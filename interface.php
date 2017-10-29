b1942f9128945b956e7f66f13d25ce20c8ce794f
wyfhouduan <32233591+wyfhouduan@users.noreply.github.com>
2017-10-29 14:33:55 +0800

    Create interface.php

---
+++ interface.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta  http-equiv="CONTENT-TYPE" ; content="text/html"  ; charset="UTF-8">
+    <title>霍格沃茨留言板</title>
+
+
+
+</head>
+<body>
+
+<form method="post" action="shujukulianjie.php">
+
+    <div style="background:url('http://4493bz.1985t.com/uploads/allimg/141227/4-14122G02344.jpg') " >
+
+
+
+        <br>
+        <br><br><br><br><br>
+        <div style="text-align: center;margin: 20px;">
+            <h1>在霍格沃茨留下你的足迹吧</h1>
+            <br>
+
+            <a href = " http://127.0.0.1/4.php " style="color:white ; " >查看留言</a>
+            <br>
+            <br>
+        </div>
+        <div style="text-align: center;margin: 20px;color: white">
+            <h2  >写下属于你的咒语</h2>
+        </div>
+
+        <div style="text-align: center;margin: 20px;color: white">
+            <td>标题：</td>
+            <input type="text" name="biaoti" value="" style="vertical-align">
+
+        </div>
+        <div style="text-align: center;margin: 20px;color: white">
+            <td>姓名：</td>
+            <input type="text" name="xingming" value="" style="vertical-align: middle;">
+        </div>
+        <br>
+
+        <div style="text-align: center;margin: 20px;font-size: 20px;color: white">
+            <label>咒语内容：</label>
+            <br>
+            <textarea name="liuyan" value="" style="width: 40%;height:80px;resize: both;"></textarea>
+        </div>
+
+        <div style="text-align: center;margin: 20px;">
+
+            <br>
+            <br>
+            <td colspan = "2" align = "center">
+                <input type = "submit" value = "提交">
+                <input type = "reset" value = "重置"></td>
+
+        </div>
+
+
+        <br><br><br><br><br><br><br><br>
+
+        <br><br><br><br><br><br><br><br>
+</form>
+
+
+
+
+</body>
+</html>
+
