868793bae1dcb34e6f48ba46cd39aa9b7028fa30
wyfhouduan <32233591+wyfhouduan@users.noreply.github.com>
2017-10-30 13:08:10 +0800

    Create create.php

---
+++ create.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+// 创建连接
+$conn = new mysqli($servername, $username, $password);
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+}
+// 创建数据库
+$sql = "CREATE DATABASE liuyanban";
+if ($conn->query($sql) === TRUE) {
+    echo "数据库创建成功";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
